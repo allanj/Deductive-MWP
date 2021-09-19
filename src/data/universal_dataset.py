44c21b9d771e2aa94153d15ba141de4015defe44
allan <allan@bytedance.com>
2021-09-20 00:07:33 +0800

    parallel label ids, collate function and compute value func

--- src/data/universal_dataset.py
+++ src/data/universal_dataset.py
@@ -1,3 +1,4 @@
+import torch
 from torch.utils.data import Dataset
 from typing import List, Union
 from transformers import PreTrainedTokenizerFast, MBartTokenizerFast
@@ -425,6 +426,97 @@ class UniversalDataset(Dataset):
                         label_ids.append([right_var_idx, left_var_idx, op_idx, is_stop])
         return label_ids
 
+
+    def get_label_ids_parallel(self, parallel_equation_layers: List[List], add_replacement: bool) -> Union[List[List[int]], None]:
+        # in this data, only have one or zero bracket
+        label_ids = []
+        num_constant = len(self.constant2id) if self.constant2id is not None else 0
+        accumulate_eqs = [0]
+        for p_idx, equation_layers in enumerate(parallel_equation_layers):
+            for l_idx, layer in enumerate(equation_layers):
+                left_var, right_var, op = layer
+                if left_var == right_var and (not add_replacement):
+                    return None
+                is_stop = 1 if l_idx == len(equation_layers) - 1 and p_idx == len(parallel_equation_layers) - 1 else 0
+                if (not left_var.startswith("m_")):
+                    if self.constant2id is not None and left_var in self.constant2id:
+                        left_var_idx = self.constant2id[left_var] + accumulate_eqs[p_idx]
+                    else:
+                        # try:
+                        assert ord(left_var) >= ord('a') and ord(left_var) <= ord('z')
+                        # except:
+                        #     print("seohting")
+                        left_var_idx = (ord(left_var) - ord('a') + num_constant + accumulate_eqs[p_idx])
+                else:
+                    _, m_p_idx, m_v_idx = left_var.split("_")
+                    m_p_idx, m_v_idx = int(m_p_idx), int(m_v_idx)
+                    # left_var_idx = -1
+                    left_var_idx = accumulate_eqs[p_idx] - accumulate_eqs[m_p_idx] - m_v_idx - 1
+                if (not right_var.startswith("m_")):
+                    if self.constant2id is not None and right_var in self.constant2id:
+                        right_var_idx = self.constant2id[right_var] + accumulate_eqs[p_idx]
+                    else:
+                        assert ord(right_var) >= ord('a') and ord(right_var) <= ord('z')
+                        right_var_idx = (ord(right_var) - ord('a') + num_constant + accumulate_eqs[p_idx])
+                else:
+                    _, m_p_idx, m_v_idx = right_var.split("_")
+                    m_p_idx, m_v_idx = int(m_p_idx), int(m_v_idx)
+                    right_var_idx = accumulate_eqs[p_idx] - accumulate_eqs[m_p_idx] - m_v_idx - 1
+                # try:
+                assert right_var_idx >= 0
+                # except:
+                #     print("right var index error")
+                #     return "right var index error"
+                # try:
+                assert left_var_idx >= 0
+                # except:
+                #     return "index error"
+
+                if left_var.startswith("m_") or right_var.startswith("m_"):
+                    if left_var.startswith("m_") and (not right_var.startswith("m_")):
+                        assert left_var_idx < right_var_idx
+                        op_idx = uni_labels.index(op)
+                        label_ids.append([left_var_idx, right_var_idx, op_idx, is_stop])
+                    elif not left_var.startswith("m_") and right_var.startswith("m_"):
+                        assert left_var_idx > right_var_idx
+                        op_idx = uni_labels.index(op + "_rev") if not op.endswith("_rev") else uni_labels.index(op[:-4])
+                        label_ids.append([right_var_idx, left_var_idx, op_idx, is_stop])
+                    else:
+                        ## both starts with m
+                        if left_var_idx >= right_var_idx: ##left larger means left m_idx smaller
+                            op = op[:-4] if left_var_idx == right_var_idx and op.endswith("_rev") else op
+                            op_idx = uni_labels.index(op)
+                            if left_var_idx > right_var_idx and (op not in ["+", "*"]):
+                                op_idx = uni_labels.index(op + "_rev") if not op.endswith("_rev") else uni_labels.index(op[:-4])
+                            label_ids.append([right_var_idx, left_var_idx, op_idx, is_stop])
+                        else:
+                            #left < right
+                            if (op in ["+", "*"]):
+                                op_idx = uni_labels.index(op)
+                                label_ids.append([left_var_idx, right_var_idx, op_idx, is_stop])
+                            else:
+                                # assert not op.endswith("_rev")
+                                assert  "+" not in op and "*" not in op
+                                op_idx = uni_labels.index(op)
+                                label_ids.append([left_var_idx, right_var_idx, op_idx, is_stop])
+                else:
+                    if left_var_idx <= right_var_idx:
+                        if left_var_idx == right_var_idx and op.endswith("_rev"):
+                            op = op[:-4]
+                        op_idx = uni_labels.index(op)
+                        label_ids.append([left_var_idx, right_var_idx, op_idx, is_stop])
+                    else:
+                        # left > right
+                        if (op in ["+", "*"]):
+                            op_idx = uni_labels.index(op)
+                            label_ids.append([right_var_idx, left_var_idx, op_idx, is_stop])
+                        else:
+                            # assert not op.endswith("_rev")
+                            op_idx = uni_labels.index(op + "_rev") if not op.endswith("_rev") else uni_labels.index(op[:-4])
+                            label_ids.append([right_var_idx, left_var_idx, op_idx, is_stop])
+            accumulate_eqs.append(accumulate_eqs[len(accumulate_eqs)-1] + len(equation_layers))
+        return label_ids
+
     def collate_function(self, batch: List[UniFeature]):
 
         max_wordpiece_length = max([len(feature.input_ids)  for feature in batch])
@@ -457,6 +549,79 @@ class UniversalDataset(Dataset):
                                   label_height_mask=np.asarray(label_height_mask))
         results = UniFeature(*(default_collate(samples) for samples in zip(*batch)))
         return results
+
+    def collate_parallel(self, batch: List[UniFeature]):
+
+        max_wordpiece_length = max([len(feature.input_ids)  for feature in batch])
+        max_num_variable = max([feature.num_variables  for feature in batch])
+        max_height = max([len(feature.labels) for feature in batch])
+        max_prev_num_intermediate = [0]
+        for h in range(max_height):
+            curr_num_intermediates = [0] # for later max(), to appear no exception, because max([]) not valid.
+            for feature in batch:
+                curr_num_intermediates.append(len(feature.labels[h]) if len(feature.labels) > h else 0)
+            max_prev_num_intermediate.append(max(curr_num_intermediates))
+        all_binary_labels = []
+        for i, feature in enumerate(batch):
+            padding_length = max_wordpiece_length - len(feature.input_ids)
+            input_ids = feature.input_ids + [self.tokenizer.pad_token_id] * padding_length
+            attn_mask = feature.attention_mask + [0]* padding_length
+            token_type_ids = feature.token_type_ids + [0]* padding_length
+            padded_variable_idx_len = max_num_variable - feature.num_variables
+            var_starts = feature.variable_indexs_start + [0] * padded_variable_idx_len
+            var_ends = feature.variable_indexs_end + [0] * padded_variable_idx_len
+            variable_index_mask = feature.variable_index_mask + [0] * padded_variable_idx_len
+
+            current_labels = []
+
+            for h_idx in range(max_height):
+
+                current_labels_at_h = []
+                current_max_prev_num_intermediate = max_prev_num_intermediate[h_idx]
+                num_var_for_comb = current_max_prev_num_intermediate + self.constant_num + max_num_variable
+                num_var_range = torch.arange(0, num_var_for_comb)
+                combination = torch.combinations(num_var_range, r=2, with_replacement=self.use_incremental_labeling) ##number_of_combinations x 2
+                num_combinations, _ = combination.size()
+                combination = combination.numpy()
+
+                if h_idx >= len(feature.labels):
+                    for _ in enumerate(combination):
+                        pad_labels_at_comb = [[0,0] for _ in enumerate(uni_labels)]
+                        current_labels_at_h.append(pad_labels_at_comb)
+                else:
+                    current_gold_labels = feature.labels[h_idx]  ## list of equations
+                    c_idx = 0
+                    for comb_idx, comb in enumerate(combination):
+                        left, right = comb
+                        current_labels_at_comb = []
+                        for label_idx, label in enumerate(uni_labels):
+                            current_labels_at_labels = [0] * 2 ## stop label
+                            if current_gold_labels[c_idx] == [left, right, label_idx, 0]:
+                                current_labels_at_labels[0] = 1
+                            elif current_gold_labels[c_idx] == [left, right, label_idx, 1]:
+                                current_labels_at_labels[1] = 1
+                            current_labels_at_comb.append(current_labels_at_labels)
+                        current_labels_at_h.append(current_labels_at_comb)
+                current_labels.append(current_labels_at_h)
+            all_binary_labels.append(current_labels)
+
+
+            padded_height = max_height - len(feature.labels)
+            label_height_mask = feature.label_height_mask + [0] * padded_height
+
+
+            batch[i] = UniFeature(input_ids=np.asarray(input_ids),
+                                attention_mask=np.asarray(attn_mask),
+                                  token_type_ids=np.asarray(token_type_ids),
+                                 variable_indexs_start=np.asarray(var_starts),
+                                 variable_indexs_end=np.asarray(var_ends),
+                                 num_variables=np.asarray(feature.num_variables),
+                                 variable_index_mask=np.asarray(variable_index_mask),
+                                 labels =np.asarray(all_binary_labels), # binary_labels: (batch_size , max_height,  num_combinations, num_op_labels, 2)
+                                  label_height_mask=np.asarray(label_height_mask))
+        results = UniFeature(*(default_collate(samples) for samples in zip(*batch)))
+        return results
+
 
 
 if __name__ == '__main__':
