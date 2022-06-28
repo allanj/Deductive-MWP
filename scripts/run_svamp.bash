5834ca660fe3e6e1126824f502b502efef80a2a5
allanj <allanmcgrady@gmail.com>
2022-06-28 15:54:49 +0800

    adding fp16

--- scripts/run_svamp.bash
+++ scripts/run_svamp.bash
@@ -20,7 +20,7 @@ for (( d=0; d<${#var_update_modes[@]}; d++  )) do
         TOKENIZERS_PARALLELISM=false \
         python3 universal_main.py --device=cuda:${dev} --model_folder=${model_folder} --mode=train --height=7 --num_epochs=1000 --consider_multiple_m0=${consider_multiple_m0} \
                           --train_file=data/mawps_asdiv-a_svamp/trainset_nodup.json --add_replacement=${add_replacement} --train_num=-1 --dev_num=-1 --var_update_mode=${var_update_mode} \
-                          --dev_file=data/mawps_asdiv-a_svamp/testset_nodup.json --test_file=none --bert_model_name=${bert_model_name} --use_constant=${use_constant}  \
+                          --dev_file=data/mawps_asdiv-a_svamp/testset_nodup.json --test_file=none --bert_model_name=${bert_model_name} --use_constant=${use_constant} --fp16=1 \
                            --learning_rate=2e-5 > logs/${model_folder}.log 2>&1
     done
 done
