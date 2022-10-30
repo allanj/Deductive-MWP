a33db5c1ce1b6b9a93ff91399c19a8e44d44ba65
allanj <allanmcgrady@gmail.com>
2022-10-30 16:24:20 +0800

    fix naming issue

--- scripts/run_gsm8k_ddp.bash
+++ scripts/run_gsm8k_ddp.bash
@@ -11,7 +11,7 @@ for (( d=0; d<${#var_update_modes[@]}; d++  )) do
     var_update_mode=${var_update_modes[$d]}
     for (( e=0; e<${#bert_model_names[@]}; e++  )) do
         bert_model_name=${bert_model_names[$e]}
-        model_folder=mathqa_${bert_model_name}_${var_update_mode}_nowamrup
+        model_folder=gsm8k_${bert_model_name}_${var_update_mode}_nowamrup
         CUDA_VISIBLE_DEVICES=0,1,2 \
         accelerate launch universal_main_ddp.py --device=cuda:0 \
                             --model_folder=${model_folder} \
@@ -32,13 +32,3 @@ for (( d=0; d<${#var_update_modes[@]}; d++  )) do
       done
 done
 
-
-
-##eval doe
-#bert_model_name=roberta-base
-#var_update_mode=gru
-#model_folder=mathqa_${bert_model_name}_${var_update_mode}
-#python3 universal_main.py --device=cuda:0 --model_folder=${model_folder} --mode=test --height=10 --train_max_height=15 --num_epochs=1000 --consider_multiple_m0=${consider_multiple_m0} \
-#                  --train_file=data/MathQA/mathqa_train_nodup.json --add_replacement=${add_replacement} --train_num=-1 --dev_num=-1 --batch_size=5 --var_update_mode=${var_update_mode} \
-#                  --dev_file=data/MathQA/mathqa_test_nodup.json --bert_model_name=${bert_model_name} --use_constant=${use_constant} --add_new_token=${add_new_token} \
-#                  --diff_param_for_height=${diff_param} --fp16=1 --filtered_steps ${filtered_steps} --parallel=1 --learning_rate=2e-5 > logs/${model_folder}.log 2>&1 &
